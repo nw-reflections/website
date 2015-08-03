703aeb700cdfcb3a3096306f37b831cfdb950c0e
Matthew Dahl <daftdahl@gmail.com>
2015-08-02 20:51:04 -0700

    GoLang experiments and HTML for landing page

---
+++ src/main/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+)
+
+// hello world, the web server
+func HelloServer(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "hello, world!\n")
+	template.CSS()
+}
+
+func main() {
+	http.HandleFunc("/hello", HelloServer)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
