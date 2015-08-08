74a8d57bcb916c56dea7e3875dea0320d8c33aed
Matthew Dahl <daftdahl@gmail.com>
2015-08-08 00:13:52 -0700

    Created Mock layout, styles, and practice routings

--- README.md
+++ README.md
@@ -8,6 +8,5 @@ I will be using this website to expand my knowledge of the Go programming langua
 
 #### Technologies Planned:
 - [`Go Programming Language`](https://golang.org/)
-- [`CouchBase Server`](http://docs.couchbase.com/developer/go-beta/introduction.html)
 - [`Go Package Template - html/template`](http://golang.org/pkg/html/template/)
 - [`SASS`](http://sass-lang.com/) and [`GoSass`](https://github.com/moovweb/gosass)
--- src/main/main.go
+++ src/main/main.go
@@ -2,21 +2,56 @@ package main
 
 import (
 	"html/template"
-	"io"
-	"log"
+	"io/ioutil"
 	"net/http"
 )
 
-// hello world, the web server
-func HelloServer(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "hello, world!\n")
-	template.CSS()
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	t, _ := template.ParseFiles("templates/" + tmpl + ".html")
+	t.Execute(w, p)
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+	renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
 }
 
 func main() {
-	http.HandleFunc("/hello", HelloServer)
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	// http.HandleFunc("/save/", saveHandler)
+	http.ListenAndServe(":8080", nil)
 }
